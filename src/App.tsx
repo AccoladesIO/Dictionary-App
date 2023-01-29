33ac306ff5c8cf3fa883085045c9e3ba19a55f7c
unknown <ononobip@gmail.com>
2023-01-29 19:32:10 +0100

    commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Hero from './components/Hero'
+import './App.scss'
+import Section from './components/Section'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Hero />
+      <Section />
+    </div>
+  )
+}
+
+export default App
