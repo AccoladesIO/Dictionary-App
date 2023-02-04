c690c1809efedca1aadf32134a9636f9891ab679
unknown <ononobip@gmail.com>
2023-02-04 23:03:20 +0100

    ref: refractored this code

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,20 +1,26 @@
 import React, { useState } from 'react'
 import { useFetch } from '../hooks/useFetch'
 
-const Search = () => {
-    
-    const {search, setSearch} = useFetch()
 
+const Search = (props: any) => {
+
+  
+    
     
 
-    const handleChange = (e: React.FormEvent) => {
-        e.preventDefault()
-        console.log(search)
+    const handleChange = (e: any) => {
+      e.preventDefault()
+      // setSearch(e.target.value)
+      props.fetchData()
+      console.log(props.search)
     }
+    
+
+    
   return (
     <div className="input">
     <form onSubmit={handleChange}>
-    <input type="text"  value={search} onChange={(e) => setSearch(e.target.value.toLowerCase())}/>
+    <input type="text" placeholder='Search a word' value={props.search} onChange={(e) => props.setSearch(e.target.value)}/>
      <input type="submit" value="Search" className='btn' />
     </form>
  </div>
--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,34 +1,83 @@
 import React, { useState } from 'react'
 import * as Fa from 'react-icons/fa'
 import './hero.scss'
-import { Data } from '../utilities/data'
-import { useFetch } from '../hooks/useFetch'
+// import { useFetch } from '../hooks/useFetch'
 import Search from './Search'
+
+
 
 const Section = () => {
 
-    const [data, setData] = useState(Data)
+
+  const [search, setSearch] = useState('')
+
+    const [word, setWord] = useState<any>([])
+    // console.log(word.phonetic, `redtfughyiojk`)
+
+
+    const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${search}`
+
+ 
+
+
+    const fetchData = async () => {
     
-    const {word, setWord} = useFetch()
-    // console.log(word)
+        try {
+            const req = await fetch(`${url}`)
+            const res = await req.json() 
+            const text = res[0]
+            console.log(text, text.word)
+            setWord(text)
+        } catch (e) {
+            console.log({e})
+        }
+    }
+
+ 
+    
+
+    console.log(word)
 
   return (
     <section className="section">
-        <Search />
+        <Search  search={search} setSearch={setSearch} fetchData={fetchData}/>
+        {word && word.word ? (
         <h1 className="heading-section-sub">
-            Word of the day: {data[`${Math.trunc(Math.random() * data.length)}`]}
+          You searched for: {word.word}
         </h1>
-        <div className="flex">
-        <p className='flex j'><Fa.FaAngleRight /> </p>
-        <p className='flex j'><Fa.FaAngleRight />Listen </p>
-            
-        </div>
+      ) : (
+        <h1 className="heading-section-sub">No data found</h1>
+      )}
 
-        <p className='flex j'><Fa.FaAngleRight />Meaning </p>
-        <p className='flex j'><Fa.FaAngleRight />Synonyms </p>
-        <p className='flex j'><Fa.FaAngleRight />Antonyms </p>
-       
-    </section>
+    {word  && word.word ? 
+   ( 
+    <>
+   <div className="flex">
+    <p className='flex j'><Fa.FaAngleRight />{word.phonetics[0]?.text} {word.phonetics[1]?.text} </p>
+    <p className='flex j'><Fa.FaAngleRight />Listen : <a href={word.phonetics[0]?.audio} target='_blank'><Fa.FaHeadphones/> </a></p>
+        
+    </div>
+
+
+      {word.meanings.map((mean: string | any) => {
+        console.log(mean)
+        return (
+          <>
+              <p className='flex j'><Fa.FaAngleRight />{mean.partOfSpeech} : {mean.definitions[0].definition}
+               </p>
+     
+              {mean.synonyms.length > 1 ? <p className='flex j'><Fa.FaAngleRight />Synonyms: {mean.synonyms[0] } {mean.synonyms[1]} </p> : null}
+              {mean.antonyms.length > 1 ? <p className='flex j'><Fa.FaAngleRight />Antonyms: {mean.antonyms[0]} {mean.antonyms} </p> : null}
+              {console.log(mean.antonyms.toString())}
+          </>
+        )
+      })}
+       </>
+   ) : null
+
+  }
+
+      </section>
   )
 }
 
